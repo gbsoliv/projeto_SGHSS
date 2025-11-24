d826a0a62aec65b7d1167d3ff272723d5649eea8
Gabriela <gbsoliveira@hotmail.com>
2025-11-24 10:52:15 -0500

    adding file "__init__.py" to all folders

--- app/repositories/pessoas_repo.py
+++ app/repositories/pessoas_repo.py
@@ -1,2 +1,1 @@
 # funções que acessam o banco de dados relacionadas a pessoas
-
--- app/routers/pessoas.py
+++ app/routers/pessoas.py
@@ -1,1 +1,19 @@
 # endpoints da API
+
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import Session
+
+from app.db.session import get_session
+from app.schemas.pessoa import PessoaCreate, PessoaRead
+from app.repositories.pessoas_repo import (
+    criar_pessoa,
+    listar_pessoas,
+    buscar_pessoa_por_email,
+)
+
+router = APIRouter(
+    prefix="/pessoas",
+    tags=["Pessoas"],
+)
