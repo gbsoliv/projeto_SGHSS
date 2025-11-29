ec0e04e87e32dc5b99a7e6eab6124a16388103a9
Gabriela <gbsoliveira@hotmail.com>
2025-11-29 17:03:49 -0500

    coding bd, schemas, repositories, routers and main for "consultas"

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.db.init_db import init_db
-from app.routers import pessoas, pacientes, profissionais_saude, administradores
+from app.routers import pessoas, pacientes, profissionais_saude, administradores, consultas
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,6 +23,7 @@ app.include_router(pessoas.router)
 app.include_router(pacientes.router)
 app.include_router(profissionais_saude.router)
 app.include_router(administradores.router)
+app.include_router(consultas.router)
 
 
 @app.get("/ping")
